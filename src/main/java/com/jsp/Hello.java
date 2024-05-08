d805e01cfaabd6aa181012487f605ee69c45d5cf
ckk914 <ckk9114@naver.com>
2024-05-09 00:52:22 +0900

    주석 수정

--- src/main/java/com/jsp/Hello.java
+++ src/main/java/com/jsp/Hello.java
@@ -7,7 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+//127.0.0.1/hello 를 웹페이지에 켜야 나타남
+//⭐️▽ 하단에 있는게 /hello 를 가능케하는 것
 @WebServlet("/hello")
 public class Hello extends HttpServlet {
 
